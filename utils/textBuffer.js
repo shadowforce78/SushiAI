006b48e82710a31a2ded21912b1d003beb9d25c6
shadowforce78 <planque.adam@gmail.com>
2025-02-19 13:31:26 +0100

    reduce maxLength in TextBuffer to 1500 and improve flush logic for better text handling

--- utils/textBuffer.js
+++ utils/textBuffer.js
@@ -1,5 +1,5 @@
 class TextBuffer {
-    constructor(maxLength = 1900) { // 1900 pour garder une marge de sécurité avec la limite Discord de 2000
+    constructor(maxLength = 1500) { // Réduit à 1500 pour plus de sécurité
         this.buffer = '';
         this.maxLength = maxLength;
     }
@@ -9,15 +9,44 @@ class TextBuffer {
     }
 
     shouldFlush() {
-        // Vérifie si on a une phrase complète et si on approche de la limite
-        return this.buffer.length >= this.maxLength || 
-               (this.buffer.length > 0 && this.buffer.match(/[.!?]\s*$/));
+        // Si le buffer dépasse la limite maximale
+        if (this.buffer.length >= this.maxLength) {
+            return true;
+        }
+
+        // Si on a une phrase complète
+        const lastPeriod = this.buffer.search(/[.!?]\s[A-Z]/);
+        return lastPeriod !== -1 && lastPeriod < this.maxLength;
     }
 
     flush() {
-        const text = this.buffer;
-        this.buffer = '';
-        return text;
+        let text = '';
+        
+        if (this.buffer.length > this.maxLength) {
+            // Chercher le dernier point ou espace avant la limite
+            const lastBreak = this.buffer.substring(0, this.maxLength).search(/[.!?\s][^\s]*$/);
+            
+            if (lastBreak !== -1) {
+                text = this.buffer.substring(0, lastBreak + 1);
+                this.buffer = this.buffer.substring(lastBreak + 1).trim();
+            } else {
+                // Si pas de point trouvé, couper au dernier espace
+                const lastSpace = this.buffer.substring(0, this.maxLength).lastIndexOf(' ');
+                if (lastSpace !== -1) {
+                    text = this.buffer.substring(0, lastSpace);
+                    this.buffer = this.buffer.substring(lastSpace + 1);
+                } else {
+                    // En dernier recours, couper à la limite
+                    text = this.buffer.substring(0, this.maxLength);
+                    this.buffer = this.buffer.substring(this.maxLength);
+                }
+            }
+        } else {
+            text = this.buffer;
+            this.buffer = '';
+        }
+
+        return text.trim();
     }
 }
 
